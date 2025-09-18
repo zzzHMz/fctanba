bca7beb94451c1f89b54c851594dcba0894963fa
zzzHmz <89087200+zzzHMz@users.noreply.github.com>
2025-09-18 12:41:36 +0700

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [{
+    name: 'discord-to-telegram',
+    script: 'src/index.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    },
+    error_file: 'logs/err.log',
+    out_file: 'logs/out.log',
+    log_file: 'logs/combined.log',
+    time: true
+  }]
+}; 
